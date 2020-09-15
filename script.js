fea3e42f9ae5b50885d445c6a00f91e1aa5dedc2
Jacob Cowan <mrjcowman@gmail.com>
2020-09-15 10:41:36 -0700

    Removed extra inactive code

--- script.js
+++ script.js
@@ -78,11 +78,6 @@ let generateRandomNumber = () => {
 }
 
 let generateRandomSpecial = () => {
-  // let specialCharacters = ["&#x21;","&#x22;","&#x23;","&#x24;","&#x25;","&#x26;","&#x27;","&#x28;",
-  //                          "&#x29;","&#x2A;","&#x2B;","&#x2C;","&#x2D;","&#x2E;","&#x2F;","&#x3A;",
-  //                          "&#x3B;","&#x3C;","&#x3D;","&#x3E;","&#x3F;","&#x40;","&#x5B;","&#x5C;",
-  //                          "&#x5D;","&#x5E;","&#x5F;","&#x60;","&#x7B;","&#x7C;","&#x7D;","&#x7E;"];
-  
   let specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
   return specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
 }
